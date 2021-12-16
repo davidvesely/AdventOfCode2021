d0a796c15893d1a66f983ca5d40713db17ef56c8
David Vesely <david.vesely@icb.bg>
2021-12-16 02:27:10 +0200

    Day 07. not very optimized

---
+++ Day07/CrabSubmarines.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    public class CrabSubmarines
+    {
+        private readonly List<int> positions;
+
+        private readonly Dictionary<int, long> fuelCost;
+
+        public CrabSubmarines(string input)
+        {
+            positions = input.Split(',').Select(int.Parse).ToList();
+            //fuelCost = positions.Distinct().ToDictionary(a => a, _ => 0L);
+            var maxPosition = positions.Max();
+            fuelCost = Enumerable.Repeat(0, maxPosition + 1)
+                .Select((_, index) => index)
+                .ToDictionary(a => a, _ => 0L);
+        }
+
+        public void CalculateLinearFuelCost()
+        {
+            foreach (var position in fuelCost.Keys)
+            {
+                long sum = 0;
+                foreach (var currentPosition in positions)
+                {
+                    sum += Math.Abs(position - currentPosition);
+                }
+
+                fuelCost[position] = sum;
+            }
+        }
+
+        public void CalculateArithmeticProgressionFuelCost()
+        {
+            foreach (var position in fuelCost.Keys)
+            {
+                long sum = 0;
+                foreach (var currentPosition in positions)
+                {
+                    int step = 1;
+                    for (int i = Math.Min(position, currentPosition); i < Math.Max(position, currentPosition); i++)
+                    {
+                        sum += step;
+                        step++;
+                    }
+                }
+
+                fuelCost[position] = sum;
+            }
+        }
+
+        public long LeastFuel => fuelCost.Min(a => a.Value);
+    }
+}
---
+++ Tests/Day07Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Day07;
+
+namespace Tests
+{
+    public class Day07Tests
+    {
+        [Test]
+        public void FirstTask()
+        {
+            var crabs = new CrabSubmarines("16,1,2,0,4,2,7,1,2,14");
+            crabs.CalculateLinearFuelCost();
+            Assert.That(crabs.LeastFuel, Is.EqualTo(37));
+        }
+
+        [Test]
+        public void SecondTask()
+        {
+            var crabs = new CrabSubmarines("16,1,2,0,4,2,7,1,2,14");
+            crabs.CalculateArithmeticProgressionFuelCost();
+            Assert.That(crabs.LeastFuel, Is.EqualTo(168));
+        }
+    }
+}
